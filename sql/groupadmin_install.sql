c31a08684b255c7ae8d33fbcc56ed9f852621d36
Edsel <edsel.lopez@jmaconsulting.biz>
2017-06-19 13:07:27 +0530

    BRES-178 Added install code to create table

---
+++ sql/groupadmin_install.sql
@@ -0,0 +1,14 @@
+--
+-- Table structure for table `civicrm_groupadmin_entity`
+--
+
+CREATE TABLE  IF NOT EXISTS `civicrm_groupadmin_entity` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT  ,
+  `group_id` int unsigned NOT NULL   COMMENT 'FK to Group ID',
+  `contact_id` int unsigned  COMMENT 'FK to contact id',
+  PRIMARY KEY (`id`),
+  CONSTRAINT FK_civicrm_groupadmin_entity_group_id FOREIGN KEY (`group_id`)
+    REFERENCES `civicrm_group`(`id`) ON DELETE CASCADE,
+  CONSTRAINT FK_civicrm_groupadmin_entity_contact_id FOREIGN KEY (`contact_id`)
+    REFERENCES `civicrm_contact`(`id`) ON DELETE CASCADE
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
